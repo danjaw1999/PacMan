88a74150480f1697cf6871ac4c4137c710ad9bca
Daniel Jaworski <daniel.jaworski1999@gmail.com>
2020-09-26 10:41:57 +0200

    Board

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Board from "./components/Board";
 
 function App() {
   const [score, setScore] = useState(0);
   return (
     <div className="App">
       <Header score={score} />
-      {/* <Board /> */}
+      <Board />
     </div>
   );
 }
