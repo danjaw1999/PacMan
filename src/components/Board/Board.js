083acd071b3ef223488bc7cc252eb92edcbc11de
Daniel Jaworski <daniel.jaworski1999@gmail.com>
2020-09-26 11:13:39 +0200

    Pacman SVG

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import "./style.css";
+import Pacman from "./../Pacman";
 class Board extends Component {
   render() {
-    return <div className="board">
-      {/* <Food/> */}
-      {/* <Pacman/> */}
-      {/* <Ghost/> */}
-      {/* <Ghost/> */}
-    </div>;
+    return (
+      <div className="board">
+        {/* <Food/> */}
+        <Pacman />
+        {/* <Ghost/> */}
+        {/* <Ghost/> */}
+      </div>
+    );
   }
 }
 export default Board;
